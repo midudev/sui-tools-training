7794ccf8069b0da1b631de6f8722197d2cc295cb
midudev <miduga@gmail.com>
2019-03-26 11:45:32 +0100

    fix(bacon/masMasFrito): change grado prop name
    BREAKING CHANGE:
    - prop grado now is called gradoDeFritura. The propType remains a number, but the name is changed
    for better readability. Please, adapt your code asap.

--- stuff/awesomeStudio/components/bacon/masMasFrito/src/index.js
+++ stuff/awesomeStudio/components/bacon/masMasFrito/src/index.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
 class BaconMasMasFrito extends Component {
   render() {
     return (
       <div className="sktest-BaconMasMasFrito">
-        <h1>BaconMasMasFrito</h1>
+        <h1>
+          BaconMasMasFrito a un grado de fritura de {this.props.gradoDeFritura}
+        </h1>
       </div>
     )
   }
@@ -15,7 +17,9 @@ BaconMasMasFrito.displayName = 'BaconMasMasFrito'
 
 // Remove these comments if you need
 // BaconMasMasFrito.contextTypes = {i18n: PropTypes.object}
-// BaconMasMasFrito.propTypes = {}
+BaconMasMasFrito.propTypes = {
+  gradoDeFritura: PropTypes.number.isRequired
+}
 // BaconMasMasFrito.defaultProps = {}
 
 export default BaconMasMasFrito
