5d5079e546f36be11b5e28d46de6d2dd61c851fb
midudev <miduga@gmail.com>
2019-03-26 11:26:33 +0100

    feat(bacon/masMasFrito): create bacon/masMasFrito component

---
+++ stuff/awesomeStudio/components/bacon/masMasFrito/src/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+// import PropTypes from 'prop-types'
+
+class BaconMasMasFrito extends Component {
+  render() {
+    return (
+      <div className="sktest-BaconMasMasFrito">
+        <h1>BaconMasMasFrito</h1>
+      </div>
+    )
+  }
+}
+
+BaconMasMasFrito.displayName = 'BaconMasMasFrito'
+
+// Remove these comments if you need
+// BaconMasMasFrito.contextTypes = {i18n: PropTypes.object}
+// BaconMasMasFrito.propTypes = {}
+// BaconMasMasFrito.defaultProps = {}
+
+export default BaconMasMasFrito
