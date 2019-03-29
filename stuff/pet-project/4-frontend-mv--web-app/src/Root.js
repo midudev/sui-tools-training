9209c8bc11f3f0b4750cb9ea0437290836bd2558
Àlex Barba <alexander.barba@gmail.com>
2019-03-29 18:52:15 +0100

    feat(META): initial commit web app

---
+++ stuff/pet-project/4-frontend-mv--web-app/src/Root.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Root = ({children}) => <div>{children}</div>
+Root.propTypes = {
+  children: PropTypes.lement
+}
+
+export default Root
