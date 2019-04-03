2b0f459dd0169d35f45e009e8ea9ae22b152d687
Serena <serenamejvaz@gmail.com>
2019-04-03 13:05:49 +0200

    perf(pages): 'change search component and add styles'

--- stuff/pet-project/4-frontend-mv--web-app/src/pages/MoviesList/index.js
+++ stuff/pet-project/4-frontend-mv--web-app/src/pages/MoviesList/index.js
@@ -1,16 +1,52 @@
 import React from 'react'
 import {Link} from 'react-router'
 import PropTypes from 'prop-types'
+import AtomImage from '@s-ui/react-atom-image'
+import AtomCard from '@s-ui/react-atom-card'
+import AtomSpinner, {AtomSpinnerTypes} from '@s-ui/react-atom-spinner'
+import './index.scss'
 
 const MoviesList = props => {
-  return props.movies.map(movie => (
-    <Link to={`/movie/${movie.imdbID}`} key={movie.imdbID}>
+  const Spinner = () => {
+    return <AtomSpinner type={AtomSpinnerTypes.FULL} />
+  }
+
+  const IMAGES = {
+    PLACEHOLDER: 'https://picsum.photos/50?image=980'
+  }
+  return props.movies.map(movie => {
+    const MovieImage = () => (
+      <div className="containerImageStyles">
+        <AtomImage
+          className="imageStyle"
+          src={movie.Poster}
+          spinner={Spinner}
+          placeholder={IMAGES.PLACEHOLDER}
+          alt="Nice Picture"
+        />
+      </div>
+    )
+
+    const MovieInfo = () => (
       <article>
-        <img src={movie.Poster} />
-        <h1>{movie.Title}</h1>
+        <h2>{movie.Title}</h2>
       </article>
-    </Link>
-  ))
+    )
+    return (
+      <Link
+        className="movieLink"
+        to={`/movie/${movie.imdbID}`}
+        key={movie.imdbID}
+      >
+        <AtomCard
+          className="movieCard"
+          media={MovieImage}
+          content={MovieInfo}
+          vertical
+        />
+      </Link>
+    )
+  })
 }
 
 MoviesList.propTypes = {
--- stuff/pet-project/4-frontend-mv--web-app/src/pages/Search/index.js
+++ stuff/pet-project/4-frontend-mv--web-app/src/pages/Search/index.js
@@ -1,63 +1,86 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import AtomInput from '@s-ui/react-atom-input'
 import Button from '@schibstedspain/sui-atom-button'
 import domain from '../../../../3-frontend-mv--lib-movies/src/index'
 import PropTypes from 'prop-types'
 import MoviesList from '../MoviesList'
-// import components from '../../../../2-frontend-mv--uilib-components/components'
+import './index.scss'
 
 const SearchPage = props => {
-  const [query, setQuery] = useState('')
+  const [movies, setMovies] = useState([])
+  const [error, setError] = useState('')
 
   const handleSubmit = e => {
     e.preventDefault()
-    props.router.push(`/?query=${query}`)
   }
 
-  const showMovies = () => props.movies && props.movies.length > 0
+  const searchMovie = query => {
+    domain
+      .get('search_movies_use_case')
+      .execute({query})
+      .then(results => {
+        if (query && results.Search) {
+          return setMovies(results.Search)
+        } else {
+          setMovies([])
+        }
+        console.log(error)
+
+        return setError('No results Found')
+      })
+  }
+  useEffect(
+    () => {
+      const {
+        location: {
+          query: {query}
+        }
+      } = props
+      domain
+        .get('search_movies_use_case')
+        .execute({query})
+        .then(results => {
+          if (query && results.Search) {
+            setMovies(results.Search)
+          } else {
+            setMovies([])
+          }
+        })
+    },
+    [props]
+  )
+
+  const handleChange = value => {
+    searchMovie(value)
+  }
+
+  const showMovies = () => movies && movies.length > 0
 
   return (
     <div>
-      <h1>SearchPage</h1>
       <form onSubmit={handleSubmit}>
         <AtomInput
           id="movies_list"
           onChange={(e, {value}) => {
-            setQuery(value)
+            handleChange(value)
           }}
         />
         <Button isSubmit type="primary">
           Search
         </Button>
       </form>
-      {showMovies() && <MoviesList movies={props.movies} />}
-      {props.error && <span>{props.error}</span>}
+      <div className="moviesSection">
+        {showMovies() && <MoviesList movies={movies} />}
+        {props.error && <span>{props.error}</span>}
+      </div>
     </div>
   )
 }
 SearchPage.propTypes = {
-  movies: PropTypes.array,
   error: PropTypes.string,
   router: PropTypes.object
 }
 
 SearchPage.renderLoading = () => <h1>Loading</h1>
-
-SearchPage.getInitialProps = ({routeInfo, context}) => {
-  const {
-    location: {
-      query: {query}
-    }
-  } = routeInfo
-  return domain
-    .get('search_movies_use_case')
-    .execute({query})
-    .then(results => {
-      if (query && results.Search) {
-        return {movies: results.Search}
-      }
-      return {error: 'No results Found'}
-    })
-}
 
 export default SearchPage
